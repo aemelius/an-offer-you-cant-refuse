7d50c04c65dba42c3587a212b12a69aa1836a20c
aemelius <simoneperfect@gmail.com>
2018-08-07 15:57:50 +0100

    Cosmetics

--- test/an_offer_you_cant_refuse/core_test.clj
+++ test/an_offer_you_cant_refuse/core_test.clj
@@ -6,9 +6,11 @@
 (expect 0 (compute-price :apple 0)) ; 0 apples are for free
 (expect 20 (compute-price :apple 1)) ; 1 apple goes full price
 (expect 20 (compute-price :apple 2)) ; 1 apple, 1 goes for free!
+
 ; for the first two apples the special offers kicks in
 ; for the third apple, full price
 (expect 40 (compute-price :apple 3))
+
 ; one full price, one free, one full, one free
 (expect 40 (compute-price :apple 4))
 
@@ -23,14 +25,19 @@
 
 ; no watermelons are also cheap
 (expect 0 (compute-price :watermelon 0))
+
 ; one goes full price...
 (expect 80 (compute-price :watermelon 1))
+
 ; ... and so do two
 (expect 160 (compute-price :watermelon 2))
+
 ; here the special offer kicks in: buy 3, pay 2
 (expect 160 (compute-price :watermelon 3))
+
 ; for the first 3 items the offer applies, the 4th watermelon goes full price
 (expect 240 (compute-price :watermelon 4))
+
 ; for the first 3 items the offer applies, the remaning 2 go full price
 (expect 320 (compute-price :watermelon 5))
 
@@ -39,3 +46,6 @@
 
 ; example of total basket price calculation
 (expect (+ 40 150 320) (total-price {:apple 4 :orange 3 :watermelon 5}))
+
+
+
