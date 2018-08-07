a903159141c4af05340cc86e36e80e61299c37e3
aemelius <simoneperfect@gmail.com>
2018-08-07 13:40:32 +0100

    Added Oranges and Watermelons

--- src/an_offer_you_cant_refuse/core.clj
+++ src/an_offer_you_cant_refuse/core.clj
@@ -1,13 +1,22 @@
 (ns an-offer-you-cant-refuse.core)
 
-(def prices {:apple 20 :orange 50 :watermelons 80})
+(def prices {:apple 20 :orange 50 :watermelon 80})
 
 (defmulti compute-price (fn [what howmany] what))
 
 (defmethod compute-price :apple [what howmany]
-  (+ (* (quot howmany 2) (:apple prices)  )
+  (+ (* (quot howmany 2) (:apple prices))
      (* (mod howmany 2) (:apple prices))
      ))
+
+(defmethod compute-price :watermelon [what howmany]
+  (+ (* (quot howmany 3) (* (:watermelon prices) 2)  ) ;; groups of 3 cost as 2
+     (* (mod howmany 3) (:watermelon prices)) ;; the remaining are full price
+     ))
+
+
+(defmethod compute-price :default [what howmany]
+  (* howmany (get prices what)))
 
 
 
--- test/an_offer_you_cant_refuse/core_test.clj
+++ test/an_offer_you_cant_refuse/core_test.clj
@@ -3,10 +3,19 @@
             [an-offer-you-cant-refuse.core :refer :all]
             [expectations :refer :all]))
 
-
-
 (expect 40 (compute-price :apple 4))
-
 (expect 20 (compute-price :apple 1))
 (expect 20 (compute-price :apple 2))
 (expect 40 (compute-price :apple 3))
+
+(expect 50 (compute-price :orange 1))
+(expect 100 (compute-price :orange 2))
+(expect 150 (compute-price :orange 3))
+
+
+(expect 80 (compute-price :watermelon 1))
+(expect 160 (compute-price :watermelon 2))
+(expect 160 (compute-price :watermelon 3))
+(expect 240 (compute-price :watermelon 4))
+
+
