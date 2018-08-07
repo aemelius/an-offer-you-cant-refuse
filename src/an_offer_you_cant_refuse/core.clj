bac42ee7d0445ce31efda8c80ea87978455c9578
aemelius <simoneperfect@gmail.com>
2018-08-07 13:25:57 +0100

    Computing special offer for apples

--- src/an_offer_you_cant_refuse/core.clj
+++ src/an_offer_you_cant_refuse/core.clj
@@ -1,5 +1,14 @@
 (ns an-offer-you-cant-refuse.core)
 
-(def prices {:apple 0.20 :orange 0.50 :watermelons 0.80})
+(def prices {:apple 20 :orange 50 :watermelons 80})
+
+(defmulti compute-price (fn [what howmany] what))
+
+(defmethod compute-price :apple [what howmany]
+  (+ (* (quot howmany 2) (:apple prices)  )
+     (* (mod howmany 2) (:apple prices))
+     ))
+
+
 
 
--- test/an_offer_you_cant_refuse/core_test.clj
+++ test/an_offer_you_cant_refuse/core_test.clj
@@ -5,3 +5,8 @@
 
 
 
+(expect 40 (compute-price :apple 4))
+
+(expect 20 (compute-price :apple 1))
+(expect 20 (compute-price :apple 2))
+(expect 40 (compute-price :apple 3))
