1874707d2e887fe4f43062bf5aae48fd52b6ce7b
aemelius <simoneperfect@gmail.com>
2018-08-07 14:27:27 +0100

    Added tests for zero items

--- test/an_offer_you_cant_refuse/core_test.clj
+++ test/an_offer_you_cant_refuse/core_test.clj
@@ -3,22 +3,26 @@
             [an-offer-you-cant-refuse.core :refer :all]
             [expectations :refer :all]))
 
+(expect 0 (compute-price :apple 0))
 (expect 20 (compute-price :apple 1))
 (expect 20 (compute-price :apple 2))
 (expect 40 (compute-price :apple 3))
 (expect 40 (compute-price :apple 4))
 
+(expect 0 (compute-price :orange 0))
 (expect 50 (compute-price :orange 1))
 (expect 100 (compute-price :orange 2))
 (expect 150 (compute-price :orange 3))
 
 
+(expect 0 (compute-price :watermelon 0))
 (expect 80 (compute-price :watermelon 1))
 (expect 160 (compute-price :watermelon 2))
 (expect 160 (compute-price :watermelon 3))
 (expect 240 (compute-price :watermelon 4))
 (expect 320 (compute-price :watermelon 5))
 
+(expect 0 (total-price {:apple 0 :orange 0 :watermelon 0}))
 (expect (+ 40 150 320) (total-price {:apple 4 :orange 3 :watermelon 5}))
 
 
