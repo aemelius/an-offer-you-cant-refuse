e906a868704b3adbd7f9cfd5dff2fadd39f9a2f9
aemelius <simoneperfect@gmail.com>
2018-08-13 17:56:14 +0100

    Added expiry date for offers

--- src/an_offer_you_cant_refuse/core.clj
+++ src/an_offer_you_cant_refuse/core.clj
@@ -1,29 +1,48 @@
 (ns an-offer-you-cant-refuse.core
-  (:gen-class))
+  (:gen-class)
+  (:import (java.text SimpleDateFormat)))
 (require '[clojure.tools.cli :refer [cli]])
 
 (def prices {:apple 20 :orange 50 :watermelon 80})
 
-(defmulti compute-price (fn [what howmany] what))
+
+
+(defn create-date [string-rep] (.parse (SimpleDateFormat. "dd.MM.yyyy")
+                                       string-rep
+                                       ))
+
+(def promotion-valid-until {:apple "12.08.2018" :watermelon "24.12.2018"} )
+
+(defn promotion-valid? [what date] (and (what promotion-valid-until) (> 1  (compare
+                                           (create-date date)
+                                           (create-date
+                                            (get promotion-valid-until what))))))
+
+
+(defmulti compute-price (fn [what howmany date] [what (promotion-valid? what date) ] ))
 
 (defn x-for-the-price-of-y [x y what howmany]
   (+ (* (quot howmany x) (* (get prices what) y))
      (* (mod howmany x) (get prices what))))
 
-(defmethod compute-price :apple [what howmany]
-  (x-for-the-price-of-y 2 1 :apple howmany))
+(defn default-price [what howmany]
+  (* howmany (get prices what)))
 
-(defmethod compute-price :watermelon [what howmany]
+(defmethod compute-price [ :apple true ] [what howmany date]
+  (x-for-the-price-of-y 2 1 :apple howmany)
+  )
+
+(defmethod compute-price [ :watermelon true ] [what howmany date]
   (x-for-the-price-of-y 3 2 :watermelon howmany))
 
 ; by default, no special offers are applied. This applies, e.g., to oranges
-(defmethod compute-price :default [what howmany]
-  (* howmany (get prices what)))
+(defmethod compute-price :default [what howmany date]
+  (default-price what howmany))
 
-(defn total-price [basket]
+(defn total-price [basket date]
   (reduce +
           (map
-           (fn [item] (compute-price (first item) (second item)))
+           (fn [item] (compute-price (first item) (second item) date))
            basket)))
 
 (defn -main [& args]
--- test/an_offer_you_cant_refuse/core_test.clj
+++ test/an_offer_you_cant_refuse/core_test.clj
@@ -3,49 +3,55 @@
    [an-offer-you-cant-refuse.core :refer :all]
    [expectations :refer :all]))
 
-(expect 0 (compute-price :apple 0)) ; 0 apples are for free
-(expect 20 (compute-price :apple 1)) ; 1 apple goes full price
-(expect 20 (compute-price :apple 2)) ; 1 apple, 1 goes for free!
+(expect 0 (compute-price :apple 0 "11.08.2018")) ; 0 apples are for free
+(expect 20 (compute-price :apple 1 "11.08.2018")) ; 1 apple goes full price
+(expect 20 (compute-price :apple 2 "11.08.2018")) ; 1 apple, 1 goes for free!
 
 ; for the first two apples the special offers kicks in
 ; for the third apple, full price
-(expect 40 (compute-price :apple 3))
+(expect 40 (compute-price :apple 3 "11.08.2018"))
 
 ; one full price, one free, one full, one free
-(expect 40 (compute-price :apple 4))
+(expect 40 (compute-price :apple 4 "11.08.2018"))
 
+;full price if promotion expired
+(expect 40 (compute-price :apple 2 "13.08.2018"))
 
 ; no oranges are cheap
-(expect 0 (compute-price :orange 0))
+(expect 0 (compute-price :orange 0 "11.08.2018"))
 
 ; buy one pay one: no special offers for oranges
-(expect 50 (compute-price :orange 1))
-(expect 100 (compute-price :orange 2))
-(expect 150 (compute-price :orange 3))
+(expect 50 (compute-price :orange 1 "11.08.2018"))
+(expect 100 (compute-price :orange 2 "11.08.2018"))
+(expect 150 (compute-price :orange 3 "11.08.2018"))
 
 ; no watermelons are also cheap
-(expect 0 (compute-price :watermelon 0))
+(expect 0 (compute-price :watermelon 0 "11.08.2018"))
 
 ; one goes full price...
-(expect 80 (compute-price :watermelon 1))
+(expect 80 (compute-price :watermelon 1 "11.08.2018"))
 
 ; ... and so do two
-(expect 160 (compute-price :watermelon 2))
+(expect 160 (compute-price :watermelon 2 "11.08.2018"))
 
-; here the special offer kicks in: buy 3, pay 2
-(expect 160 (compute-price :watermelon 3))
+; here the special offer kicks in: buy 3, pay 2...
+(expect 160 (compute-price :watermelon 3 "11.08.2018"))
+
+
+; ... unless the promotion expired
+(expect 240 (compute-price :watermelon 3 "11.08.2019"))
 
 ; for the first 3 items the offer applies, the 4th watermelon goes full price
-(expect 240 (compute-price :watermelon 4))
+(expect 240 (compute-price :watermelon 4 "11.08.2018"))
 
 ; for the first 3 items the offer applies, the remaning 2 go full price
-(expect 320 (compute-price :watermelon 5))
+(expect 320 (compute-price :watermelon 5 "11.08.2018"))
 
 ; an empty basket is free
-(expect 0 (total-price {:apple 0 :orange 0 :watermelon 0}))
+(expect 0 (total-price {:apple 0 :orange 0 :watermelon 0} "11.08.2018"))
 
 ; example of total basket price calculation
-(expect (+ 40 150 320) (total-price {:apple 4 :orange 3 :watermelon 5}))
+(expect (+ 40 150 320) (total-price {:apple 4 :orange 3 :watermelon 5} "11.08.2018"))
 
 
 
